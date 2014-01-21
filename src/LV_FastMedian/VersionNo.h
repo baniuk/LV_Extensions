ad9047a1418a796849f1a9950d77db3bf6bfed9b
Baniukiewicz <baniuk@zut.edu.pl>
2014-01-21 21:56:01 +0100

    init

---
+++ src/LV_FastMedian/VersionNo.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------
+  VERSION CONTROL BUILD SYSTEM                               
+  This header file was created by VERBUILD v1.0.1            
+  -----------------------------------------------------------
+  help : verbuild -?                                         
+  info : http://www.yeamaec.com                              
+         yeamaec@hanafos.com ,krkim@yeamaec.com              
+-----------------------------------------------------------*/
+
+#ifndef VERSIONNO__H
+#define VERSIONNO__H
+
+#define VERSION_FULL           0.10.1020.27
+
+#define VERSION_BASEYEAR       2013
+#define VERSION_DATE           "2014-01-21"
+#define VERSION_TIME           "19:50:06"
+
+#define VERSION_MAJOR          0
+#define VERSION_MINOR          10
+#define VERSION_BUILDNO        1020
+#define VERSION_EXTEND         27
+
+#define VERSION_FILE           0,10,1020,27
+#define VERSION_PRODUCT        0,10,1020,27
+#define VERSION_FILESTR        "0,10,1020,27"
+#define VERSION_PRODUCTSTR     "0,10,1020,27"
+
+#endif
