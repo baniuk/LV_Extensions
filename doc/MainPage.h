6eaa1f4dfc1a88c572124a8594be1a6b6aef7679
Baniukiewicz <baniuk@zut.edu.pl>
2014-01-24 09:40:48 +0100

    MainPage for doxygen doc added

---
+++ doc/MainPage.h
@@ -0,0 +1,8 @@
+/**
+ * \mainpage "General informations"
+ * \section s1 "Exportable methods"
+ * \subsection ss1 LV_Tiff
+ * Short descriptio of exportable methods.
+ * \copydoc Tiff_GetParams
+ * \copydoc Tiff_GetImage
+ */ 
