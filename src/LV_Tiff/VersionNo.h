a92879de5e96e583325ef8be1f23e3bb01532779
Baniukiewicz <baniuk@zut.edu.pl>
2014-01-27 20:34:31 +0100

    Fixed FastMedian tests Upgraded version for LV_Tiff

--- src/LV_Tiff/VersionNo.h
+++ src/LV_Tiff/VersionNo.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           0.10.26.116
+#define VERSION_FULL           1.0.26.117
 
 #define VERSION_BASEYEAR       2014
 #define VERSION_DATE           "2014-01-27"
-#define VERSION_TIME           "20:16:26"
+#define VERSION_TIME           "20:24:17"
 
-#define VERSION_MAJOR          0
-#define VERSION_MINOR          10
+#define VERSION_MAJOR          1
+#define VERSION_MINOR          0
 #define VERSION_BUILDNO        26
-#define VERSION_EXTEND         116
+#define VERSION_EXTEND         117
 
-#define VERSION_FILE           0,10,26,116
-#define VERSION_PRODUCT        0,10,26,116
-#define VERSION_FILESTR        "0,10,26,116"
-#define VERSION_PRODUCTSTR     "0,10,26,116"
+#define VERSION_FILE           1,0,26,117
+#define VERSION_PRODUCT        1,0,26,117
+#define VERSION_FILESTR        "1,0,26,117"
+#define VERSION_PRODUCTSTR     "1,0,26,117"
 
 #endif
